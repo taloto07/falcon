761418db42cf1e1370ad4e6be32a411a26e92c33
taloto07 <taloto07@gmail.com>
2014-08-10 18:19:25 -0700

    more validator sample

--- FalconTowncar/src/com/falcon/hosting/test/validator/ValidateCar.java
+++ FalconTowncar/src/com/falcon/hosting/test/validator/ValidateCar.java
@@ -7,8 +7,6 @@ import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 
-import com.falcon.hosting.doa.User;
-
 public class ValidateCar {
 	public static void main(String[] args){
 		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
@@ -16,7 +14,7 @@ public class ValidateCar {
 		
 		Car car = new Car();
 		car.setManufacturer("toyota");
-		car.setLicensePlate("adfa");
+		car.setLicensePlate("a");
 		car.setSitCount(2);
 		car.setPassword("12345678");
 		car.setConfirmPassword("23456789");
@@ -27,7 +25,9 @@ public class ValidateCar {
 			System.out.println("Car is valid.");
 		}else{
 			for (ConstraintViolation<Car> cv: validatorCar){
+				System.out.println(cv.getPropertyPath().toString());
 				System.out.println(cv.getMessage());
+				
 			}
 		}
 	}
