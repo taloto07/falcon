76bccc2d1d11422e0aa86ac472385c6066caba1e
Alvaro Licea, Jr. <Senor_guero@msn.com>
2014-08-12 16:42:41 -0700

    Made the HistoryServlet add in string template

--- FalconTowncar/src/com/falcon/hosting/servlet/HistoryServlet.java
+++ FalconTowncar/src/com/falcon/hosting/servlet/HistoryServlet.java
@@ -1,5 +1,49 @@
 package com.falcon.hosting.servlet;
 
-public class HistoryServlet {
-	public static int ayy = 0;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.stringtemplate.v4.ST;
+import org.stringtemplate.v4.STGroup;
+
+public class HistoryServlet extends BaseServlet {
+	private static final long serialVersionUID = 1L;
+	private final String errorMessage = "No current records at this time.";
+    
+    public HistoryServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HistoryProcess(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HistoryProcess(request, response);
+	}
+
+	protected void HistoryProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html");
+		
+		String contextPath = this.getContextPath();
+		
+		PrintWriter out = response.getWriter();
+		
+		STGroup templates = this.getSTGroup();
+		ST page = templates.getInstanceOf("template");
+		ST body = null;
+				
+		
+		page.add("contextPath", contextPath);
+		page.add("title", "History");
+		page.add("body", body.render());
+		
+		out.print(page.render());
+		out.flush();
+	}
 }
