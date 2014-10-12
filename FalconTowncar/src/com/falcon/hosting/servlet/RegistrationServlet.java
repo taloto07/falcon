9d15dc68b51cc4de059b10542e5df2ebeaa21e69
Alvaro Licea, Jr. <Senor_guero@msn.com>
2014-10-12 16:35:09 -0700

    Allows user to be registered to DB & redirects to Login page.

--- FalconTowncar/src/com/falcon/hosting/servlet/RegistrationServlet.java
+++ FalconTowncar/src/com/falcon/hosting/servlet/RegistrationServlet.java
@@ -62,6 +62,56 @@ public class RegistrationServlet extends BaseServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//process data from from
+		if(processInfo(request, response))
+			//redirect back to login page
+			response.sendRedirect("/FalconTowncar/login");
+		else {
+			//Emails / Passwords did not match
+			String errorMessage = "Passwords or Emails didn't match";
+			response.setCharacterEncoding("UTF-8");
+			response.setContentType("text/html");
+			
+			String contextPath = this.getContextPath();
+			
+			PrintWriter out = response.getWriter();
+			
+			STGroup templates = this.getSTGroup();
+			ST page = templates.getInstanceOf("template");
+			ST body = templates.getInstanceOf("registration");
+			
+			// add javascript to template
+			ST script = templates.getInstanceOf("javascript");
+			script.add("contextPath", contextPath);
+			script.add("filename", "bootstrap-datepicker.js");
+			String datePicker = script.render();
+					
+			script.remove("filename");
+			script.add("filename", "start-date-picker-script.js");
+			String startDatePicker = script.render();
+			// end adding javascript
+			
+			List<State> states = service.getAllStateASC();
+			for (State state: states){
+				state.setAbbreviation(state.getAbbreviation().toUpperCase());
+			}
+			
+			page.add("script", datePicker + startDatePicker);
+
+			body.add("errorMessage", errorMessage);
+			body.add("contextPath", contextPath);
+			page.add("contextPath", contextPath);
+			page.add("title", "Registration");
+			body.add("states", states);
+			page.add("body", body.render());
+			
+			out.print(page.render());
+			out.flush();
+		}
+	}
+	
+	private boolean processInfo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//store all data from the form in String variables
 		String firstName = request.getParameter("firstname");
 		String lastName = request.getParameter("lastname");
 		String email = request.getParameter("email");
@@ -69,16 +119,20 @@ public class RegistrationServlet extends BaseServlet {
 		String inputPassword = request.getParameter("password");
 		String inputConfirmPassword = request.getParameter("confirmPassword");
 		String phoneNumber = request.getParameter("phoneNumber");
-		
-		System.out.println(firstName);
-		System.out.println(lastName);
-		System.out.println(email);
-		System.out.println(confirmEmail);
-		System.out.println(inputPassword);
-		System.out.println(inputConfirmPassword);
-		System.out.println(phoneNumber);
-		
-		response.sendRedirect("/FalconTowncar/login");
+		//validate password/email match
+		if(! inputPassword.equals(inputConfirmPassword))
+			return false;
+		else if(! email.equals(confirmEmail))
+			return false;
+		//create a new user with the database attributes
+		User u = new User();
+		u.setFirstname(firstName);
+		u.setLastname(lastName);
+		u.setEmail(email);
+		u.setPassword(inputPassword);
+		u.setPhoneNumber(phoneNumber);
+		//attempt to add user to the database
+		return service.addUser(u);
 	}
-
+	
 }
